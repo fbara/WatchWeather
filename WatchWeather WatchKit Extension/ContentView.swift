2957dfdda3aef0646d8868ba4293cd9d480ddb1e
fbara <fbara1@yahoo.com>
2021-09-17 10:52:43 -0500

    Initial Commit

---
+++ WatchWeather WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchWeather WatchKit Extension
+//
+//  Created by Frank Bara on 9/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchWeather WatchKit Extension/WatchWeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchWeatherApp.swift
+//  WatchWeather WatchKit Extension
+//
+//  Created by Frank Bara on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
